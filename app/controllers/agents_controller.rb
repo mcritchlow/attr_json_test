7027c65e60c95bf87726d292ce48c89c74c929dc
Matt Critchlow <mcritchlow@ucsd.edu>
2018-09-14 11:25:09 -0700

    Add rest of CRUD for agents

--- app/controllers/agents_controller.rb
+++ app/controllers/agents_controller.rb
@@ -9,18 +9,32 @@ class AgentsController < ApplicationController
   end
 
   def new
+    @agent = type_class.new
   end
 
   def edit
   end
 
   def create
+    @agent = type_class.new(agent_params)
+    if @agent.save
+      redirect_to @agent, notice: "#{type} was successfully created."
+    else
+      render action: 'new'
+    end
   end
 
   def update
+    if @agent.update(agent_params)
+      redirect_to @agent, notice: "#{type} was successfully updated."
+    else
+      render action: 'edit'
+    end
   end
 
   def destroy
+    @agent.destroy
+    redirect_to agents_url
   end
 
   private
@@ -39,5 +53,11 @@ class AgentsController < ApplicationController
 
   def type_class
     type.constantize
+  end
+
+  def agent_params
+    params.require(type.underscore.to_sym).permit(:label, :alternateLabel,
+                                                  :orcid, :note,
+                                                  :identifier, :type)
   end
 end
